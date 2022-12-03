1b8f51d806d29a1afede6daceb4b58f8342a0593
PradnyaTawar <tawar.rani96@gmail.com>
2022-12-03 22:21:00 +0530

    carts

---
+++ code.js
@@ -0,0 +1,28 @@
+function validateForm() {
+    let email = document.getElementById("email").value;
+    let password = document.getElementById("password").value;
+    if (email === 'admin@admin.com' && password === '123456') {
+        alert('"Login successful"')
+
+    } else (
+        alert('Incorrect email or password')
+    )
+
+}
+
+
+let items = document.querySelectorAll('.carousel .carousel-item')
+
+items.forEach((el) => {
+    const minPerSlide = 5
+    let next = el.nextElementSibling
+    for (var i=1; i<minPerSlide; i++) {
+        if (!next) {
+            // wrap carousel by using first child
+        	next = items[0]
+      	}
+        let cloneChild = next.cloneNode(true)
+        el.appendChild(cloneChild.children[0])
+        next = next.nextElementSibling
+    }
+})
