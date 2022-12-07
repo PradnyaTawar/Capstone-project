f52fdaa006e56778da43bf5b3e4b5b352c8cf298
PradnyaTawar <tawar.rani96@gmail.com>
2022-12-07 15:41:35 +0530

    price and title

--- js/code.js
+++ js/code.js
@@ -14,9 +14,9 @@ function validateForm() {
 let items = document.querySelectorAll('.carousel .carousel-item ')
 
 items.forEach((el) => {
-    const minPerSlide = 6
+    const minPerSlide = 5
     let next = el.nextElementSibling
-    for (var i=1; i<minPerSlide; i++) {
+    for (var i=0; i<minPerSlide; i++) {
         if (!next) {
             // wrap carousel by using first child
         	next = items[0]
